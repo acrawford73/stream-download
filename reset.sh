1ef6d4f4f24f85b7881581948c1ce473483656b4
acrawford73 <anthonycrawford@outlook.com>
2021-06-19 02:03:40 -0300

    .

--- reset.sh
+++ reset.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 ./stream.py -p
-FILE=playback.m3u8
-if test -f "$FILE"; then
-	rm $FILE
-	echo "$FILE deleted."
-else
-	echo "$FILE not found."
-fi
-echo ""
 
 if test -d "video"; then
 	rm -rf video
